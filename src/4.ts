cb7edb2bac8aed76033050cc16495eab0f939389
Ruslan Dolibskyi <dolibskiyruslan.gmail.com>
2024-06-30 10:46:12 +0300

    HW 07

--- src/4.ts
+++ src/4.ts
@@ -1,7 +1,65 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  public readonly name: string;
+
+  constructor(name: string, key: Key) {
+    this.name = name;
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.name} entered the house.`);
+    } else {
+      console.log("The door is closed.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is now open.");
+    } else {
+      console.log("The key does not match.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
-const person = new Person(key);
+const person = new Person("Homer", key);
 
 house.openDoor(person.getKey());
 
